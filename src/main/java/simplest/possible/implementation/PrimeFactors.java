8bbafaab116397ef1d3fb215135382d7280c0999
Sowmya Muduganti <si@Sowmyas-MBP.Dlink>
2021-01-04 15:56:56 +0530

    first commit

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,16 @@
+package simplest.possible.implementation;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class PrimeFactors {
+
+    public List<Integer> of(int number) {
+        List<Integer> primeFactors = emptyList();
+        if(number==1){
+            return emptyList();
+        }
+        return primeFactors;
+    }
+}
